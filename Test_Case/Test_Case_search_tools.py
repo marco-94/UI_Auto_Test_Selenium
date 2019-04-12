780c16eb38e9584a4b131ec979af3a8369599513
marco-94 <liangshanchi@126.ocm>
2019-04-12 18:06:08 +0800

    新增类型筛选用例

--- Test_Case/Test_Case_search_tools.py
+++ Test_Case/Test_Case_search_tools.py
@@ -76,4 +76,28 @@ class Test(unittest.TestCase):
                 self.browser.find_elements_by_xpath("//*[@id=\"tool\"]/span[1]/div/descendant::a")[j].click()
             self.save_img('时间筛选')
 
+    @BeautifulReport.add_test_img('类型筛选')
+    def scroll_to_the_bottom(self):
+        u"""类型筛选"""
+        self.browser.find_element_by_xpath("//*[@id=\"query\"]").send_keys("微信")
+        self.browser.find_element_by_xpath("//*[@id=\"searchForm\"]/div/input[3]").click()
+        time.sleep(2)
+        self.browser.find_element_by_xpath("//*[@id=\"tool_show\"]/a").click()
+        time.sleep(1)
+        for i in range(0, 10):
+            j = random.randint(0, 3)
+            self.browser.find_element_by_xpath("//*[@id=\"type\"]").click()
+            if j == 0:
+                self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+            elif j == 1:
+                self.browser.find_element_by_xpath("//*[@id=\"check_pic\"]").click()
+                self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+            elif j == 2:
+                self.browser.find_element_by_xpath("//*[@id=\"check_video\"]").click()
+                self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+            else:
+                self.browser.find_element_by_xpath("//*[@id=\"check_pic\"]").click()
+                self.browser.find_element_by_xpath("//*[@id=\"check_video\"]").click()
+                self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+
 
