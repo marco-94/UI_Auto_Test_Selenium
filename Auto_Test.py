cc7ddc5b50bec03d3da00d352eb664ac502fc055
liangshanchi <liangshanchi@126.com>
2019-06-13 14:41:41 +0800

    update

--- Auto_Test.py
+++ Auto_Test.py
@@ -4,9 +4,9 @@ from BeautifulReport import BeautifulReport
 
 if __name__ == '__main__':
     test_suite = unittest\
-        .defaultTestLoader.discover("E:/test/auto_test_local/Auto_Test/Test_Case", pattern="Test*.py")
+        .defaultTestLoader.discover("E:/test/Auto_Test/Test_Case", pattern="Test_Case_WeChat*.py")
     result = BeautifulReport(test_suite)
     result\
         .report(filename='测试报告-搜狗微信',
                 description='搜狗微信',
-                log_path='E:/test/auto_test_local/Auto_Test/Test_Result/Test_Report')
+                log_path='E:/test/Auto_Test/Test_Result/Test_Report')
