2154c4e746cdc8e71118df9d67f9ca070dceac27
liangshanchi <liangshanchi@126.com>
2019-08-19 09:47:49 +0800

    新增日志功能

--- Test_Case/Test_Case_Search_Tools.py
+++ Test_Case/Test_Case_Search_Tools.py
@@ -56,6 +56,7 @@ class Test(unittest.TestCase):
 
     @BeautifulReport.add_test_img('时间筛选')
     def test_select_time_search(self):
+        logger.info("执行测试用例：时间筛选")
         u"""时间筛选"""
         self.browser.find_element_by_xpath("//*[@id=\"query\"]").send_keys("微信")
         logger.info("搜索框输入关键词“微信”")
@@ -68,14 +69,15 @@ class Test(unittest.TestCase):
         logger.info("展开搜索工具")
         time.sleep(1)
 
-        for i in range(0, 20):
+        for i in range(0, 10):
             try:
                 self.browser.find_element_by_xpath("//*[@id=\"time\"]").click()
                 logger.info("展开时间搜索框")
                 time.sleep(1)
 
                 total = len(self.browser.find_elements_by_xpath("//*[@id=\"tool\"]/span[1]/div/descendant::a"))
-                if i ==0:
+                if i == 0:
+                    # 第一次检索，不让输入或选择自定义时间
                     j = random.randint(0, (total-2))
                 else:
                     j = random.randint(0, (total-1))
@@ -98,7 +100,6 @@ class Test(unittest.TestCase):
                         date_list = BasePage(self.browser)\
                             .get_every_day(datetime.date.today() - relativedelta(months=1), datetime.date.today())
 
-                        # 输入自定义开始时间
                         starttime = self.browser.find_element_by_xpath("//*[@id=\"date_start\"]")
                         starttime.clear()
                         logger.info("清空开始时间输入框")
@@ -106,7 +107,6 @@ class Test(unittest.TestCase):
 
                         logger.info("输入开始时间：%s", date_list[0])
 
-                        # 输入自定义结束时间
                         endtime = self.browser.find_element_by_xpath("//*[@id=\"date_end\"]")
                         endtime.clear()
                         logger.info("清空结束时间输入框")
@@ -114,16 +114,16 @@ class Test(unittest.TestCase):
 
                         logger.info("输入结束时间：%s", date_list[1])
 
-                        # 点击确认按钮
                         starttime.click()
                         endtime.click()
                         self.browser.find_elements_by_xpath("//*[@id=\"tool\"]/span[1]/div/descendant::a")[j]\
                             .click()
                         logger.info("点击确认按钮，进行搜索")
 
-                    # 选择自定义时间
+                        logger.info("选择自定义时间")
                     elif k == 1:
-                        """自定义开始时间"""
+                        logger.info("自定义开始时间")
+
                         self.browser.find_element_by_xpath("//*[@id=\"date_start\"]").click()
                         logger.info("展开开始时间月份表")
 
@@ -134,7 +134,6 @@ class Test(unittest.TestCase):
                         btn_prev_month = random.randint(0, 2)
 
                         if btn_prev_month == 0:
-                            # 不翻页
                             datelist = BasePage(self.browser).select_every_day(start_month)
 
                             w = random.randint(0, len(datelist[1])-1)
@@ -178,12 +177,11 @@ class Test(unittest.TestCase):
                         btn_prev_month_end = random.randint(0, 2)
 
                         if btn_prev_month_end == 0:
-                            # 不翻页
                             datelist = BasePage(self.browser).select_every_day(end_month)
 
                             w = random.randint(0, len(datelist[1])-1)
                             self.browser.find_elements_by_xpath(end_month)[w].click()
-                            logger.info("选择时间：%s-%s", arrow.now().format("YYYY-MM"), datelist[1][w])
+                            logger.info("选择结束时间：%s-%s", arrow.now().format("YYYY-MM"), datelist[1][w])
 
                         elif btn_prev_month_end == 1:
                             self.browser.find_element_by_xpath("/html/body/div[7]/div[1]/a[1]").click()
@@ -193,13 +191,12 @@ class Test(unittest.TestCase):
 
                             w = random.randint(0, len(datelist[0])-1)
                             self.browser.find_elements_by_xpath(end_month)[w].click()
-                            logger.info("选择时间：%s-%s", arrow.now().shift(months=-1).format("YYYY-MM"), datelist[0][w])
+                            logger.info("选择结束时间：%s-%s", arrow.now().shift(months=-1).format("YYYY-MM"), datelist[0][w])
 
                             self.browser.find_element_by_xpath("/html/body/div[7]/div[1]/a[2]").click()
                             logger.info("选择完时间之后，复位到当前月份")
 
                         elif btn_prev_month_end == 2:
-                            # 翻到上上月
                             BasePage(self.browser).double_click("/html/body/div[7]/div[1]/a[1]")
                             logger.info("翻到上上月")
 
@@ -207,17 +204,16 @@ class Test(unittest.TestCase):
 
                             w = random.randint(0, len(datelist[0])-1)
                             self.browser.find_elements_by_xpath(end_month)[w].click()
-                            logger.info("选择时间：%s-%s", arrow.now().shift(months=-2).format("YYYY-MM"), datelist[0][w])
+                            logger.info("选择结束时间：%s-%s", arrow.now().shift(months=-2).format("YYYY-MM"), datelist[0][w])
 
                             BasePage(self.browser).double_click("/html/body/div[7]/div[1]/a[2]")
                             logger.info("选择完时间之后，复位到当前月份")
 
-                        # 点击确认按钮
                         self.browser.find_elements_by_xpath("//*[@id=\"tool\"]/span[1]/div/descendant::a")[j]\
                             .click()
+                        logger.info("点击确认按钮")
 
             except ElementNotVisibleException:
-                # 如果开始时间大于结束时间，本次循环结束，进入下次循环
                 self.browser.refresh()
                 logger.info("开始时间大于结束时间，刷新页面")
 
@@ -226,86 +222,104 @@ class Test(unittest.TestCase):
 
     @BeautifulReport.add_test_img('类型筛选')
     def test_select_type_search(self):
+        logger.info("执行测试用例：类型筛选")
         u"""类型筛选"""
+        
         self.browser.find_element_by_xpath("//*[@id=\"query\"]").send_keys("微信")
+        logger.info("搜索框输入关键词“微信”")
+
         self.browser.find_element_by_xpath("//*[@id=\"searchForm\"]/div/input[3]").click()
+        logger.info("点击搜索按钮")
+
         time.sleep(5)
         self.browser.find_element_by_xpath("//*[@id=\"tool_show\"]/a").click()
+        logger.info("展开搜索工具")
         time.sleep(1)
 
         for i in range(0, 10):
             j = random.randint(0, 3)
             self.browser.find_element_by_xpath("//*[@id=\"type\"]").click()
+            logger.info("点击搜索类型按钮")
 
-            # 不勾选任意类型
             if j == 0:
                 self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+                logger.info("不勾选任意类型")
 
-            # 仅筛选图集类型
             elif j == 1:
                 self.browser.find_element_by_xpath("//*[@id=\"check_pic\"]").click()
+                logger.info("仅筛选图集类型")
                 self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+                logger.info("确认搜索")
 
-            # 仅筛选含视频类型
             elif j == 2:
                 self.browser.find_element_by_xpath("//*[@id=\"check_video\"]").click()
+                logger.info("仅筛选含视频类型")
                 self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+                logger.info("确认搜索")
 
-            # 选中图集和含视频类型
             else:
                 self.browser.find_element_by_xpath("//*[@id=\"check_pic\"]").click()
+                logger.info("选中图集类型")
                 self.browser.find_element_by_xpath("//*[@id=\"check_video\"]").click()
+                logger.info("选中含视频类型")
                 self.browser.find_element_by_xpath("//*[@id=\"type_enter\"]").click()
+                logger.info("确认搜索")
 
             self.save_img('类型筛选')
+            logger.info("保存截图：类型筛选")
 
     @BeautifulReport.add_test_img('公众号内检索')
     def test_public_address_search(self):
+        logger.info("执行测试用例：公众号内检索")
         u"""公众号内检索"""
+
         self.browser.find_element_by_xpath("//*[@id=\"query\"]").send_keys("微信")
         self.browser.find_element_by_xpath("//*[@id=\"searchForm\"]/div/input[3]").click()
 
-        # 获取页面数据总条数
         total = len(self.browser.find_elements_by_xpath("//*[@id=\"main\"]/div[4]/ul/descendant::li"))
+        logger.info("获取页面数据总条数")
 
-        # 获取当前页面全部的站点信息,放入public_address_list
         j = 0
         public_address_list = []
         while j < total:
             public_address_list. \
                 append(self.browser.find_elements_by_xpath("//*[@id=\"main\"]/div[4]/ul/descendant::a")[2 + 5 * j].text)
             j += 1
+        logger.info("获取当前页面全部的站点信息,放入public_address_list")
         time.sleep(1)
 
-        # 展开搜索工具
         self.browser.find_element_by_xpath("//*[@id=\"tool_show\"]/a").click()
+        logger.info("展开搜索工具")
         time.sleep(1)
 
-        # 展开公众号输入框
         self.browser.find_element_by_xpath("//*[@id=\"search\"]").click()
+        logger.info("展开公众号输入框")
         time.sleep(1)
 
         for i in range(0, 10):
             try:
                 k = random.randint(0, (len(public_address_list)-1))
 
-                # 输入public_address_list内的公众号信息
                 self.browser.find_element_by_xpath("//*[@id=\"tool\"]/span[5]/div/form/span/input"). \
                     send_keys(public_address_list[k])
+                logger.info("输入public_address_list内的公众号信息")
                 time.sleep(1)
 
-                # 点击检索按钮
                 self.browser.find_element_by_xpath("//*[@id=\"search_enter\"]").click()
                 time.sleep(3)
-                self.save_img('公众号内检索')
+                logger.info("点击检索按钮")
 
-                # 清除输入信息
+                self.save_img('公众号内检索')
+                logger.info("保存截图：公众号内检索")
+
                 self.browser.find_element_by_xpath("//*[@id=\"search\"]").click()
                 search_tool = self.browser.find_element_by_xpath("//*[@id=\"tool\"]/span[5]/div/form/span/input")
+                logger.info("清除输入信息")
                 search_tool.clear()
 
             except ElementNotVisibleException:
                 print(self.browser.find_element_by_xpath("//*[@id=\"tool\"]/span[5]/div/form/p").text)
+                logger.info("没有找到相应的公众号")
                 continue
 
 
